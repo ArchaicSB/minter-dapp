a85a029782d4c6a02001217414f3ed31edbeac07
ArchaicSB <106488341+ArchaicSB@users.noreply.github.com>
2022-06-03 20:15:35 -0700

    updated colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
 const contractAddress = "0xEE9f6752bD320cA14974A3702751DD85c860E39d"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Archaic Shell Babies NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Archaic Shell Babies NFT Project is sustainability-focused, and dedicated to saving the Sea Turtles. We are partnering with Ocean Clean-up programs, Sea Turtle Conservation, and sustainable Products. The Archaic Shell Babies are here to take over the web, rid the world plastic every single day, and save a couple more Sea Turtles. Welcome To ASB. Lets Ride This Wave Together.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const h1_public_mint = "Public Minting Is Finally Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_public_mint = "No whitelist for this project. PUBLIC MINT IS OPEN! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint Your Archaic Shell Babies NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
